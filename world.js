a2817f7cdb862b83659c3390c78d232dcbbbd6b2
Tori-Anne <connell_tori@yahoo.com>
2018-11-19 21:18:22 -0500

    exercise 4

--- world.js
+++ world.js
@@ -2,9 +2,11 @@
 window.onload = function() {	
 	let country = document.getElementById("country").value;
     let lookupButton = document.getElementById("lookup");
+	let allCheckBox = document.getElementById("all");
 	
 	lookupButton.addEventListener("click", function() {
 		let request = new XMLHttpRequest();
+		let URL = '';
 		request.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
 				document.getElementById("result").innerHTML = request.responseText;
@@ -13,7 +15,15 @@ window.onload = function() {
 				alert('There was a problem with the request.');
 				}
 		};
-		request.open('GET', 'world.php?country=' + country, true);
+		
+		if (allCheckBox.checked == true) {
+			URL = 'world.php?all=true';
+		}
+		else {
+			URL = 'world.php?country=' + country;
+		}			
+			
+		request.open('GET', URL, true);
 		request.send();
 	});
 	
